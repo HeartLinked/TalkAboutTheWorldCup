9abcc2655df6d29d5f8bddabd417e5ce737e09cb
lfyhl0907 <lfyhl0907@gmail.com>
2022-09-07 11:27:09 +0800

    upgrade loginUI 2.0

--- app/src/main/java/com/ss/video/rtc/demo/quickstart/LoginActivity.java
+++ app/src/main/java/com/ss/video/rtc/demo/quickstart/LoginActivity.java
@@ -26,7 +26,7 @@ import java.util.regex.Pattern;
  * - 展示当前 SDK 使用的版本号 {@link RTCEngine#getSdkVersion()}
  *
  * 有以下常见的注意事项：
- * 1.SDK必要的权限有：外部内存读写、摄像头权限、麦克风权限，其余完整的权限参见{@link src/main/AndroidManifest.xml}。
+ * 1.SDK必要的权限有：外部内存读写、摄像头权限、麦克风权限，其余完整的权限参见{@link /main/AndroidManifest.xml}。
  * 没有这些权限不会导致崩溃，但是会影响SDK的正常使用。
  * 2.SDK 对房间名、用户名的限制是：非空且最大长度不超过128位的数字、大小写字母、@ . _ \ -
  */
@@ -46,8 +46,8 @@ public class LoginActivity extends AppCompatActivity {
         });
         // 获取当前SDK的版本号
         String SDKVersion = RTCEngine.getSdkVersion();
-        TextView versionTv = findViewById(R.id.version_tv);
-        versionTv.setText(String.format("VolcEngineRTC v%s", SDKVersion));
+//        TextView versionTv = findViewById(R.id.version_tv);
+//        versionTv.setText(String.format("VolcEngineRTC v%s", SDKVersion));
 
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO},
